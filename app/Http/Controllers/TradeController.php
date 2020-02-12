048e166d148be376b04d67e3ff377bcec3fad077
wordshack <wordshack04@gmail.com>
2020-02-11 23:04:09 -0600

    ordenada por categoria

--- app/Http/Controllers/TradeController.php
+++ app/Http/Controllers/TradeController.php
@@ -10,8 +10,9 @@ class TradeController extends Controller
     public function index()
     {
         $Trade=Type::select('name','subtype','sellprice','buyprice','traderCat')
-            ->where('buyprice','!=','-1')->
-            get();
+            ->where('buyprice','!=','-1')
+            ->orderBy('subtype','asc')
+            ->get();
         return view('front.trader',compact("Trade"));
     }
 }
